58349c8d03739bd94db1ad38592552b71d371015
Windi <windi@hacktiv8.com>
2016-12-19 16:12:29 +0700

    Template

---
+++ recursive.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// release 0
+let prime_factors = (angka, hasil = []) => {
+  // write your code here
+}
+
+console.log(prime_factors(3)); // [3]
+console.log(prime_factors(6)); // [2,3]
+console.log(prime_factors(8)); // [2,2,2]
+console.log(prime_factors(25)); // [5,5]
+console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
+
+// release 1
+let simple_recursive = (number) => {
+  // write your code here
+}
+
+console.log(simple_recursive(39)); // 4
+console.log(simple_recursive(999)); // 2
+console.log(simple_recursive(3)); // 3
