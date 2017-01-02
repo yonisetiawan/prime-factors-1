c884f45a96891953dfe4a370ac2f03e5b9945283
Dell Inc. &   "name": null <pair+dell.eri@devbootcamp.com>
2017-01-02 21:10:16 +0700

    1st_onProgress

--- recursive.js
+++ recursive.js
@@ -1,21 +1,31 @@
 "use strict";
 
-// release 0
-let prime_factors = (angka, hasil = []) => {
-  // write your code here
-}
-
-console.log(prime_factors(3)); // [3]
-console.log(prime_factors(6)); // [2,3]
-console.log(prime_factors(8)); // [2,2,2]
-console.log(prime_factors(25)); // [5,5]
-console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
+// // release 0
+// let prime_factors = (angka, hasil = []) => {
+//   // write your code here
+//
+// }
+//
+// console.log(prime_factors(3)); // [3]
+// console.log(prime_factors(6)); // [2,3]
+// console.log(prime_factors(8)); // [2,2,2]
+// console.log(prime_factors(25)); // [5,5]
+// console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 
 // release 1
 let simple_recursive = (number) => {
   // write your code here
+  if (number > 100){
+    return 1;
+  }else{
+    number = number.toString()
+    number = number.split("");
+    var numberbaru = number[0]*number[1]
+    console.log(numberbaru);
+  }
+  return simple_recursive(Number(numberbaru));
 }
 
 console.log(simple_recursive(39)); // 4
-console.log(simple_recursive(999)); // 2
-console.log(simple_recursive(3)); // 3
+// console.log(simple_recursive(999)); // 2
+// console.log(simple_recursive(3)); // 3
