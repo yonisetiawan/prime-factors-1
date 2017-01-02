fa112eeecece7485917cee873138b98eb11f682f
Didit Suryadi & Achmad Kamil <pair+didietsuryadi.kahmadsyt@devbootcamp.com>
2017-01-02 21:12:59 +0700

    rekursif finish

--- recursive.js
+++ recursive.js
@@ -3,6 +3,21 @@
 // release 0
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+   var angkaPembantu1 = 2;
+   var angkaPembantu2 = 1;
+
+  while (angkaPembantu1 < angka){
+    if(angka % angkaPembantu1 === 0){
+      hasil.push(angkaPembantu1);
+      angka = angka / angkaPembantu1;
+    }
+    else{
+      angkaPembantu1 = angkaPembantu1+angkaPembantu2;
+      angkaPembantu2 = 2;
+    }
+  }
+  hasil.push(angkaPembantu1);
+  return hasil;
 }
 
 console.log(prime_factors(3)); // [3]
@@ -14,6 +29,20 @@ console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 // release 1
 let simple_recursive = (number) => {
   // write your code here
+  var jumlah = 1;
+  var angka = String(number);
+
+
+  if(angka.length === 1){
+    return number;
+  }
+  if(angka.length>1){
+    for(var i=0; i<angka.length; i++){
+      jumlah *= angka[i];
+    }
+  }
+return simple_recursive(jumlah);
+
 }
 
 console.log(simple_recursive(39)); // 4
