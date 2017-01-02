5c93991b6cffda89f5e0f930bdbd4311e964f7b1
Samue PH Sinaga & Izhharuddin Syanmil <pair+samuelphsinaga.syanmil@devbootcamp.com>
2017-01-02 17:12:43 +0700

    prime factor with multiple for loop

--- recursive.js
+++ recursive.js
@@ -1,13 +1,31 @@
 "use strict";
 
 // release 0
+function isPrime(num) {
+    for(var i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function primegenerator(num){
+  var angkaprima = []
+  for (var i = 2; i <= num; i++){
+    if (isPrime(i) == true){
+      angkaprima.push(i)
+    }
+  }
+  return angkaprima
+}
+
 let prime_factors = (angka, hasil = []) => {
   // write your code here
-  var prime = [2,3,5,7,11]
-  for (var i = 0; i < prime.length; i++){
-    if (angka % prime[i] == 0){
-      angka /= prime[i]
-      hasil.push(prime[i])
+  var angkaprima = primegenerator(angka)
+  for (var i = 0; i < angkaprima.length; i++){
+    if (angka % angkaprima[i] == 0){
+      angka /= angkaprima[i]
+      hasil.push(angkaprima[i])
       i--
     }
   }
@@ -18,8 +36,7 @@ console.log(prime_factors(3)); // [3]
 console.log(prime_factors(6)); // [2,3]
 console.log(prime_factors(8)); // [2,2,2]
 console.log(prime_factors(25)); // [5,5]
-console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
-
+// console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 // release 1
 let simple_recursive = (number) => {
   // write your code here
